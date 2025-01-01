b0f0202ac4fbcc0851ec027f07754e0419c97675
hdhankecha <95201259+hdhankecha@users.noreply.github.com>
2025-01-01 17:04:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { MailchimpForm } from "./components/MailchimpForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <MailchimpForm />
     </div>
   );
 }
